beba4eb772f77318a87b7f83e8eb32739013acf0
Arthur <61190726+arthurbohan@users.noreply.github.com>
2020-05-14 16:47:33 +0300

    Add md format (#11)
    * add md format
    
    * use rubocop and correct offenses
    
    * try to fix conflicts
    
    Co-authored-by: Arthur <arthurbokhan@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # rubocop:disable Rails/OutputSafety
+  def markdown(text)
+    options = %i[hard_wrap autolink no_intra_emphasis fenced_code_blocks]
+    Markdown.new(text, *options).to_html.html_safe
+  end
+  # rubocop:enable Rails/OutputSafety
 end
