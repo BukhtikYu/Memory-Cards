e549af65ecadf01da6a2147965d0c84870bda66a
AnastasiyaGavrilik <53380940+AnastasiyaGavrilik@users.noreply.github.com>
2020-06-01 17:28:30 +0300

    Add feauture test for update passwords page (#59)
    Co-authored-by: Nastassia Haurylik <nastassiahaurylik@MacBook-Air-Nastassia.local>

---
+++ spec/features/update_password_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe 'Update password', type: :feature do
+  let(:user) { FactoryBot.create :user }
+
+  before do
+    log_in(user)
+    visit '/en/users/edit'
+    click_link 'Change your password'
+  end
+
+  context 'when user fill correct password' do
+    it 'returns success message on root page' do
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button('Update User')
+
+      expect(page).to have_text('Hello world')
+      expect(page).to have_current_path '/en'
+    end
+  end
+
+  context 'when new password empty' do
+    it "returns error message with: Password can't be blank" do
+      click_button('Update User')
+
+      expect(page).to have_text("Password can't be blank")
+      expect(page).to have_text('1 error prohibited this user from being saved')
+    end
+  end
+
+  context 'when password confirmation empty' do
+    it "returns error message with: Password confirmation doesn't match Password" do
+      fill_in 'user_password', with: 'password'
+      click_button('Update User')
+
+      expect(page).to have_text('1 error prohibited this user from being saved')
+      expect(page).to have_text("Password confirmation doesn't match Password")
+    end
+  end
+
+  context 'when password does not match with password confirmation' do
+    it "returns error message: Password confirmation doesn't match Password" do
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'user password'
+      click_button('Update User')
+
+      expect(page).to have_text("Password confirmation doesn't match Password")
+      expect(page).to have_text('1 error prohibited this user from being saved')
+    end
+  end
+end
--- spec/support/capybara_config.rb
+++ spec/support/capybara_config.rb
@@ -3,7 +3,7 @@ module ::Selenium::WebDriver::Remote
   class Bridge
     alias_method :old_execute, :execute
     def execute(*args)
-      sleep(0.1)
+      sleep(0.2)
       old_execute(*args)
     end
   end
