72a441d8702ca130960f27f6cb4e470560c728e4
AnastasiyaGavrilik <53380940+AnastasiyaGavrilik@users.noreply.github.com>
2020-06-03 10:32:40 +0300

    Add request test for error pages (#70)
    Co-authored-by: Nastassia Haurylik <nastassiahaurylik@MacBook-Air-Nastassia.local>

--- spec/requests/errors_request_spec.rb
+++ spec/requests/errors_request_spec.rb
@@ -3,4 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Errors', type: :request do
+  it 'respond with 404 if page not found' do
+    get '/404'
+    expect(response).to have_http_status(:not_found)
+  end
+
+  it 'respond with 422 if page unacceptable' do
+    get '/422'
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+
+  it 'respond with 500 if page have internal error' do
+    get '/500'
+    expect(response).to have_http_status(:error)
+  end
 end
